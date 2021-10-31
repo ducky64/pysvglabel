a0bdf0b57197bfb6d561704bbc5236a5f4ce04a2
Richard Lin <elpato25@gmail.com>
2021-10-31 00:30:10 -0700

    skeleton

---
+++ pysvglabel.py
@@ -0,0 +1,15 @@
+import argparse
+import csv
+
+parser = argparse.ArgumentParser(description='Create label sheets from SVG templates.')
+parser.add_argument('template', type=str,
+                    help='Input SVG template file.')
+parser.add_argument('csv', type=str,
+                    help='Input CSV data file.')
+parser.add_argument('output', type=str,
+                    help="Filename to write outputs to. Must end in .svg."
+                         + " If multiple sheets are needed, appends a prefix to the second one, starting with _2."
+                         + " (future versions may auto-detect output format by the extension)")
+args = parser.parse_args()
+
+reader = csv.DictReader(args.csv)
