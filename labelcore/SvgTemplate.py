ff2fa7fec600682d48e4bdbdde66a8ab8fa0b470
Richard Lin <elpato25@gmail.com>
2021-11-10 01:51:55 -0800

    string interpolation

--- labelcore/SvgTemplate.py
+++ labelcore/SvgTemplate.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from typing import Any, Dict, Callable, Optional
+from typing import Any, Dict, Callable, Optional, cast
 from copy import deepcopy
 
 from labelfrontend import LabelSheet
@@ -29,9 +29,11 @@ class SvgTemplate:
 
   def __init__(self, root: ET.ElementTree):
     self.root = root
-    self.env: Optional[Dict[str, Any]] = None
+    self.env: Dict[str, Any] = cast(Any, None)
+    self.sheet: LabelSheet = cast(Any, None)
 
     def replace_start(elt: ET.Element) -> None:
+      # TODO support flowRoot/flowPara in starting block
       for child in elt.findall('svg:text', NAMESPACE):
         child_text = text_of(child)
         if child_text.startswith('🏁'):
@@ -46,16 +48,40 @@ class SvgTemplate:
             raise BadTemplateException("sheet not defined in starting block")
           if not isinstance(self.env['sheet'], LabelSheet):
             raise BadTemplateException(f"sheet not instance of LabelSheet, got {self.env['sheet']}")
+          self.sheet = cast(LabelSheet, self.env['sheet'])
 
           elt.remove(child)  # remove the init block from the template
 
     self.visit(self.root.getroot(), replace_start)
 
-
-  def create_sheet(self) -> ET.ElementTree:  # TODO proper ETree type
+  def create_sheet(self) -> ET.ElementTree:
     raise NotImplementedError
 
   def apply(self, row: Dict[str, str]) -> ET.Element:
     new = deepcopy(self.root.getroot())
-    raise NotImplementedError
+    value_variables = {key: value for key, value in row.items()
+                       if key.isidentifier()}
+    self.env.update(value_variables)
+    self.env.update({'row': row})
+
+    def process_text(elt: ET.Element) -> None:
+      for child in elt.findall('svg:tspan', NAMESPACE):
+        process_text(child)
+      for child in elt.findall('svg:flowPara', NAMESPACE):
+        process_text(child)
+      if elt.text:
+        old_text = elt.text
+        elt.text = eval(f'f"""{elt.text}"""', self.env)  # TODO proper escaping, even though """ in a label is unlikely
+        print(f"{old_text} -> {elt.text}")
+
+    def apply_template(elt: ET.Element) -> None:
+      for child in elt.findall('svg:text', NAMESPACE):
+        if not text_of(child).startswith('🐍'):
+          process_text(child)
+      for child in elt.findall('svg:flowRoot', NAMESPACE):
+        if not text_of(child).startswith('🐍'):
+          process_text(child)
+
+    self.visit(new, apply_template)
+
     return new
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -7,3 +7,5 @@ from labelcore import SvgTemplate
 class SimpleLabelTestCase(unittest.TestCase):
   def test_simple(self) -> None:
     template = SvgTemplate(ET.parse("test_1.75x0.5.svg"))
+
+    template.apply({'barcode': '1', 'id': '1', 'description': 'one'})
