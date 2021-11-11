ecdcd4f5abd05929b45953f899d9960c442b65a8
Richard Lin <elpato25@gmail.com>
2021-11-11 14:13:04 -0800

    refactor test cases

---
+++ tests/LabelTestCase.py
@@ -0,0 +1,12 @@
+import unittest
+
+import xml.etree.ElementTree as ET
+from typing import List
+
+
+class LabelTestCase(unittest.TestCase):
+  def write_labels(self, sheets: List[ET.Element]):
+    for i, sheet in enumerate(sheets):
+      with open(f'out/{self.__class__.__name__}_{self._testMethodName}_{i}.svg', 'wb') as file:
+        root = ET.ElementTree(sheet)
+        root.write(file)
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,21 +1,16 @@
-import unittest
 import csv
 
 import xml.etree.ElementTree as ET
 from labelcore import SvgTemplate
+from LabelTestCase import LabelTestCase
 
 
-class SimpleLabelTestCase(unittest.TestCase):
+class SimpleLabelTestCase(LabelTestCase):
   def test_simple(self) -> None:
     with open('testdata.csv', newline='') as csvfile:
       reader = csv.DictReader(csvfile)
       table = [row for row in reader]
     template = SvgTemplate(ET.parse("test_1.75x0.5.svg"))
 
-    tpi = template.apply_instance(table[0], table, 0)
-
     sheets = template.apply_table(table)
-    for i, sheet in enumerate(sheets):
-      with open(f'out/{self.__class__.__name__}_{self._testMethodName}_{i}.svg', 'wb') as file:
-        root = ET.ElementTree(sheet)
-        root.write(file)
+    self.write_labels(sheets)
