96bda5ff0beb677db971423f558d7ad2e7a07abc
Richard Lin <elpato25@gmail.com>
2021-11-11 16:46:48 -0800

    svg test case w/o impl

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -8,10 +8,10 @@ from LabelTestCase import LabelTestCase
 
 class SimpleLabelTestCase(LabelTestCase):
   def test_simple(self) -> None:
-    with open('testdata.csv', newline='') as csvfile:
+    with open('test_simple.csv', newline='') as csvfile:
       reader = csv.DictReader(csvfile)
       table = [row for row in reader]
-    template = SvgTemplate(ET.parse("test_1.75x0.5.svg"))
+    template = SvgTemplate(ET.parse("simple_1.75x0.5.svg"))
 
     sheets = template.apply_table(table)
     self.write_labels(sheets)
---
+++ tests/test_subsvg.py
@@ -0,0 +1,21 @@
+import csv
+
+import xml.etree.ElementTree as ET
+from labelcore import SvgTemplate, NAMESPACES
+from labelcore.SvgTemplate import get_text_of
+from LabelTestCase import LabelTestCase
+
+
+class SubSvgTestCase(LabelTestCase):
+  def test_simple(self) -> None:
+    with open('test_subsvg.csv', newline='') as csvfile:
+      reader = csv.DictReader(csvfile)
+      table = [row for row in reader]
+    template = SvgTemplate(ET.parse("test_subsvg.svg"))
+
+    sheets = template.apply_table(table)
+    self.write_labels(sheets)
+
+    self.assertEqual(len(sheets), 1)
+    groups = sheets[0].findall('svg:g', NAMESPACES)
+    self.assertEqual(len(groups), 4)
