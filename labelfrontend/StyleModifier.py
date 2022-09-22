81b4cb123608b13253adda67cad11d75806194ce
Richard Lin <elpato25@gmail.com>
2022-09-22 00:19:12 -0700

    Support opacity in fill and stroke color

--- labelfrontend/StyleModifier.py
+++ labelfrontend/StyleModifier.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from typing import List
+from typing import List, Dict
 
 from labelcore import SvgTemplate, filter_text_elts
 from labelcore.GroupReplacer import GroupReplacer
@@ -9,22 +9,23 @@ class StyleModifier(GroupReplacer):
   """
   A generic modifier that changes a style attribute of all elements in the group
   """
-  def __init__(self, attrib_name: str, attrib_value: str):
+  def __init__(self, attribs: Dict[str, str]):
     """
-    :param attrib_name: attribute name, eg 'fill'
-    :param attrib_value: attrib value, eg 'ffffff'
+    :param attribs: dict of attrib names to values, eg '{'fill': '#ffffff'}'
     """
-    self.attrib_name = attrib_name
-    self.attrib_value = attrib_value
+    self.attribs = attribs
 
   def process_group(self, context: SvgTemplate, elts: List[ET.Element]) -> List[ET.Element]:
     for elt in elts:
       if 'style' not in elt.attrib:
         elt.attrib['style'] = ''
       style_contents = elt.attrib['style'].split(';')
-      style_contents = [elt for elt in style_contents  # discard the existing key
-                        if not elt.startswith(f'{self.attrib_name}:')]
-      style_contents.append(f'{self.attrib_name}:{self.attrib_value}')  # and tack new value at the end
+      style_pairs = [elt.split(':') for elt in style_contents]
+      style_pairs = [elt for elt in style_pairs
+                     if elt[0] not in self.attribs.keys()]  # discard existing keys
+      for attrib_name, attrib_value in self.attribs.items():
+        style_pairs.append([attrib_name, attrib_value])  # and tack new values at the end
+      style_contents = [':'.join(elt) for elt in style_pairs]
       elt.attrib['style'] = ';'.join(style_contents)
 
     return elts
@@ -33,20 +34,32 @@ class StyleModifier(GroupReplacer):
 class FillColor(StyleModifier):
   """
   Changes the fill color of objects within this group.
+  If the color is specified with transparency (as consistent with the Inkscape color UI),
+  this additionally maps the fill-opacity attribute.
   """
   def __init__(self, color: str):
     """
     :param color: new color
     """
-    super().__init__('fill', color)
+    if color.startswith('#') and len(color) == 9:  # need to separately map opacity
+      opacity = int(color[8:9], 16)
+      super().__init__({'fill': color[:8], 'fill-opacity': str(opacity / 255)})
+    else:
+      super().__init__({'fill': color})
 
 
 class StrokeColor(StyleModifier):
   """
   Changes the fill color of objects within this group.
+  If the color is specified with transparency (as consistent with the Inkscape color UI),
+  this additionally maps the stroke-opacity attribute.
   """
   def __init__(self, color: str):
     """
     :param color: new color
     """
-    super().__init__('stroke', color)
+    if color.startswith('#') and len(color) == 9:  # need to separately map opacity
+      opacity = int(color[8:9], 16)
+      super().__init__({'stroke': color[:8], 'stroke-opacity': str(opacity / 255)})
+    else:
+      super().__init__({'stroke': color})
--- tests/test_color.py
+++ tests/test_color.py
@@ -16,7 +16,7 @@ class TextColorTestCase(LabelTestCase):
     self.write_label(sheet)
 
     groups = sheet.findall('svg:g', NAMESPACES)[0].findall('svg:g', NAMESPACES)
-    self.assertEqual(len(groups), 5)
+    self.assertEqual(len(groups), 6)
 
     # TODO text_of should add newlines?
     # ignoring type is needed here to avoid boilerplate assertions that find returns not-None
@@ -25,3 +25,6 @@ class TextColorTestCase(LabelTestCase):
     self.assertIn('fill:#0000ff', groups[2][0][0].find('svg:flowRoot', NAMESPACES).attrib['style'])  # type: ignore
     self.assertIn('fill:#ffff00', groups[3][0][0].find('svg:flowRoot', NAMESPACES).attrib['style'])  # type: ignore
     self.assertIn('fill:#00ffff', groups[4][0][0].find('svg:flowRoot', NAMESPACES).attrib['style'])  # type: ignore
+
+    self.assertIn('fill:#ff0000', groups[5][0][0].find('svg:flowRoot', NAMESPACES).attrib['style'])  # type: ignore
+    self.assertIn('fill-opacity:0', groups[5][0][0].find('svg:flowRoot', NAMESPACES).attrib['style'])  # type: ignore
